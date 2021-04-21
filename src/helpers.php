e17d7cc72e61d1886afdc251664e4929a5b87e7e
frederick tyteca <frederick@tyteca.net>
2021-04-21 12:06:04 +0200

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -4,9 +4,12 @@ use Dotenv\Dotenv;
 
 function sldconfig($key)
 {
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-    $dotenv->load();
-    return $_ENV[strtoupper($key)] ?? $_ENV[$key];
+    static $dotenv = null;
+    if (!is_object($dotenv)) {
+        $dotenv = Dotenv::createMutable(__DIR__ . '/..');
+        $dotenv->load();
+    }
+    return $_ENV[strtoupper($key)];
 }
 
 function generateSignature($attributes)
