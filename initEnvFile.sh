e6857bed0d219e9af8746503fc985c573d4964c8
frederick tyteca <frederick@tyteca.net>
2021-04-21 11:03:10 +0200

    script to create env file from env on CI

---
+++ initEnvFile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp .env-sample .env
+for varName in SLD_PARTENAIRE_ID SLD_DOMAIN_URL CLEF_SECRETE CE_SOCIETE CE_NOM CE_PRENOM CE_EMAIL CE_CODEPOSTAL CE_VILLE; do
+    sed -i 's/\('"$varName"'*= *\).*/\1'"${!varName}"'/' .env
+done
+cat .env
