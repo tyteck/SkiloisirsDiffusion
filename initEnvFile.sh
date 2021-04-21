4a271b28251e3a995232a9531b921c72e79fff7c
frederick tyteca <frederick@tyteca.net>
2021-04-21 11:43:01 +0200

    wip

--- initEnvFile.sh
+++ initEnvFile.sh
@@ -2,6 +2,6 @@
 
 cp .env-sample .env
 for varName in SLD_PARTENAIRE_ID SLD_DOMAIN_URL CLEF_SECRETE CE_SOCIETE CE_NOM CE_PRENOM CE_EMAIL CE_CODEPOSTAL CE_VILLE; do
-    sed -i 's#${varName}#'"${!varName}"'#' .env
+    sed -i 's#'="${varName}"'#'="${!varName}"'#' .env
 done
 cat .env
